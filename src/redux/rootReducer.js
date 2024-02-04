763526f512bea24ed1d5190f05ac9ddc1f574e60
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-02-04 17:50:20 +0200

    add Redux Persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,22 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './filter/filterSlice';
-
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-//   blacklist: ['filter'],
-// };
 
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export default persistedReducer;
 
 export default rootReducer;
